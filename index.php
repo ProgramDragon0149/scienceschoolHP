123439626b0bb4d5aaecdc47a88ba1b7a6d7365e
ProgramDragon0149 <programdragon0149@gmail.com>
2021-10-22 23:58:26 +0900

    index.htmlを消去しました

--- index.php
+++ index.php
@@ -10,23 +10,36 @@
       <a href="https://www.google.com/?hl=ja"><h5>仮想大学ホームページへ</h5></a>
     </div>
     <div class="body-container">
-      <header>
-        <div class="header-image">
+      <header>   <!-- 「ヘッダーイメージ」から「メニューバー」まで -->
+        <div class="header-image"> <!-- ヘッダーイメージ -->
           <div class="logo">
             <p class="logo-upper">仮想大学　学生プロジェクト</p>
             <p class="logo-lower">つどえ！サイエンスクール</p>
           </div>
         </div>
-        <div class="menubar">
-          
+        <div class="menubar">  <!-- メニューバー -->
+          <ul>
+            <a href="#"><li class="menu">「サイエンスクール」とは？</li></a>
+            <a href="#"><li class="menu">実際の活動風景</li></a>
+            <a href="#"><li class="menu">ご依頼・ご意見</li></a>
+          </ul>
         </div>
       </header>
 
       <div class="main">
+        <div class="topics">
+          <div class="topics-container">
+            <span><p>トピックス</p></span>
+            <?php 
+              
+            ?>
+          </div>
+        </div>
       </div>
 
       <footer>
       </footer>
     </div>
+    <script src=script.js></script>
   </body>
 </html>
